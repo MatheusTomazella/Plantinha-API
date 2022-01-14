a72df98ffef785ca721947bb5a538fcf01a5bdbc
Matheus Tomazella <matheus250504@gmail.com>
2022-01-14 01:22:00 -0300

    Deploy Change

--- src/Database.ts
+++ src/Database.ts
@@ -19,6 +19,7 @@ class Database {
         this.sequelize = new Sequelize(process.env.DB_DATABASE||"", process.env.DB_USERNAME||"", process.env.DB_PASSWORD||"", {
             host: process.env.DB_HOST||"",
             dialect: 'mysql',
+            dialectModule: require('mysql2'),
             port: parseInt(process.env.DB_PORT||"3306"),
         });
     }
